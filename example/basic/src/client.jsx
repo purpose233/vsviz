c49d0d06ed9519bcdf967363d02a0c5bba627860
purpose233 <zhangsu7133@qq.com>
2019-06-17 22:00:01 +0800

    chore(ui): finally figure out how to handle with webworker

---
+++ example/basic/src/client.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render } from 'react-dom'
+import { WSLoader, Custom2DCanvas } from '../../../packages/ui/lib/index';
+
+class MyCanvas extends Custom2DCanvas {
+  renderCanvas(loaderData, canvas, context) {
+    console.log(loaderData);
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.loader = new WSLoader('ws://localhost:3000/');
+  }
+
+  render () {
+    return (
+      <MyCanvas 
+        loader={this.loader}
+        dataIds={['canvas0']}
+        width={800}
+        height={600}
+      />
+    );
+  }
+}
+
+render(<App />, document.getElementById('container'));
+
+// const ws = new WebSocket('ws://localhost:3000/');
+
+// ws.onopen = function () {
+//   console.log('ws is opened');
+// };
+// ws.onclose = function () {
+//   console.log('ws is closed');
+// }
+// ws.onmessage = function (msg) {
+//   console.log('ws receive message: ', msg);
+// };
---
+++ example/basic/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  entry: path.resolve(__dirname, './src/client.jsx'),
+
+  devtool: 'sourcemap',
+
+  watch: true,
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'client.js'
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
