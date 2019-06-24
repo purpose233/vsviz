593b30c281df2ba1d0bfe71c9b57327800f8a092
purpose233 <zhangsu7133@qq.com>
2019-06-24 11:27:01 +0800

    feat(ui): add canvas2dinteract widget

---
+++ packages/ui/src/components/Canvas2DInteractWidget.tsx
@@ -0,0 +1,26 @@
+import { Canvas2D } from './Canvas2DWidget';
+
+export abstract class Canvas2DInteract extends Canvas2D {
+  
+  // when onCanvasInit is overrided, super.onCanvasInit need to be called
+  protected async onCanvasInit(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D): Promise<void> {
+    canvas.onclick     = (e) => { this.onCanvasClick(e, canvas, ctx); };
+    canvas.onmousedown = (e) => { this.onCanvasMouseDown(e, canvas, ctx); };
+    canvas.onmousemove = (e) => { this.onCanvasMouseMove(e, canvas, ctx); };
+    canvas.onmouseup   = (e) => { this.onCanvasMouseUp(e, canvas, ctx); };
+    canvas.onmouseover = (e) => { this.onCanvasMouseOver(e, canvas, ctx); };
+    canvas.onmouseout  = (e) => { this.onCanvasMouseOut(e, canvas, ctx); };
+  }
+
+  protected async onCanvasClick(e: MouseEvent, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {};
+
+  protected async onCanvasMouseDown(e: MouseEvent, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {};
+
+  protected async onCanvasMouseMove(e: MouseEvent, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {};
+
+  protected async onCanvasMouseUp(e: MouseEvent, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {};
+
+  protected async onCanvasMouseOver(e: MouseEvent, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {};
+
+  protected async onCanvasMouseOut(e: MouseEvent, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {};
+}
---
+++ packages/ui/src/components/Canvas2DWidget.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { ConnectComponent } from './connectComponent';
+import { BaseWidgetPropsType } from '../common/types';
+import { BaseWidget } from './baseWidget';
+import { ParsedDataType } from '@vsviz/builder';
+
+export type Canvas2DPropsType = BaseWidgetPropsType & {
+  width: number,
+  height: number
+}
+
+// TODO: enable to set width & height by meta data
+
+export abstract class Canvas2D extends BaseWidget<Canvas2DPropsType> {
+
+  private canvas: HTMLCanvasElement;
+  private canvasCtx: CanvasRenderingContext2D;
+  private isRAFEnabled: boolean = true;
+
+  protected async renderCanvasOnRAF(canvas: HTMLCanvasElement,
+                                    ctx: CanvasRenderingContext2D): Promise<void> {};
+
+  protected async renderCanvasOnData(loaderData: Map<string, ParsedDataType>, 
+                                     canvas: HTMLCanvasElement, 
+                                     ctx: CanvasRenderingContext2D): Promise<void> {};
+
+  protected async onCanvasInit(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D): Promise<void> {}
+
+  public enableRAF(): void {
+    if (this.isRAFEnabled) { return; }
+    this.isRAFEnabled = true;
+    requestAnimationFrame(this.runRAF);
+  }
+
+  public disableRAF(): void {
+    this.isRAFEnabled = false;
+  }
+
+  private runRAF = async (): Promise<void> => {
+    if (!this.isRAFEnabled) { return; }
+
+    if (this.canvas && this.canvasCtx) {
+      await this.renderCanvasOnRAF(this.canvas, this.canvasCtx);
+    }
+    requestAnimationFrame(this.runRAF);
+  }
+
+  private onCanvasLoad = async (ref: HTMLCanvasElement) => {
+    this.canvas = ref;
+    this.canvasCtx = this.canvas.getContext('2d');
+
+    const { width, height } = this.props;
+    this.canvas.width = width;
+    this.canvas.height = height;
+    await this.onCanvasInit(this.canvas, this.canvasCtx);
+    requestAnimationFrame(this.runRAF);
+  }
+
+  protected clearCanvas() {
+    this.canvas.width = this.props.width;
+  }
+
+  public renderNodes(loaderData: Map<string, ParsedDataType>): React.ReactNode {
+    if (this.canvas && this.context) {
+      this.renderCanvasOnData(loaderData, this.canvas, this.canvasCtx);
+    }
+    return (
+      <div>
+        <canvas ref={this.onCanvasLoad} />
+      </div>
+    );
+  }
+
+  // TODO: maybe put render in BaseWidget
+  public render(): React.ReactNode {
+    return (
+      <ConnectComponent 
+        loader={this.props.loader}
+        dataIds={this.props.dataIds}
+        renderNodes={this.renderNodes.bind(this)}
+        onInit={this.onInit.bind(this)}
+      />
+    )
+  }
+}
