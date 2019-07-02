f37d21da9ae8c874e432212a83939184e6f90b08
purpose233 <zhangsu7133@qq.com>
2019-07-02 16:59:57 +0800

    fix(server): export event enum for function middleware

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,5 @@
+export { SessionEventEnum, TimerEventEnum } from './common/constants';
+
 // TODO: unite the name of Timer and Data
 export { SessionMiddleware } from './middlewares/sessionMiddleware';
 export { TimerMiddleware } from './middlewares/timerMiddleware';
