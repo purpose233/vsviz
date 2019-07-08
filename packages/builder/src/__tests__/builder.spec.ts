68d6c169513cef6e30c0dc1160a6274c3a629686
purpose233 <zhangsu7133@qq.com>
2019-07-08 16:59:34 +0800

    chore(builder): update test case

--- packages/builder/src/__tests__/builder.spec.ts
+++ packages/builder/src/__tests__/builder.spec.ts
@@ -1,5 +1,15 @@
 import { validateDataInfo } from '../common/serialize';
+import { DataInfoType } from '../common/types';
 
-test("Judge data info", () => {
-  expect(validateDataInfo(null)).toBe(false);
+const headerInfo: DataInfoType = {
+  id: 'test001',
+  streamType: 'customed',
+  dataType: 'json',
+  size: 10,
+  sequence: 0,
+  timestamp: 1110
+}
+
+test('validate valid header', () => {
+  expect(validateDataInfo(headerInfo)).toBe(true);
 });
